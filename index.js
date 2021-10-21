eedce102c81ef955d5cf3b614c1ef5fab18a7c74
jeremy Dubrulle <https://github.com/jeremy93130/jour3>
2021-10-21 11:05:53 +0200

    Sliders

--- index.js
+++ index.js
@@ -1,28 +1,53 @@
 $(function () {
+    var index = 0
 
-    var slideIndex = 1;
+    $('.point').click(function () {
+        index = ($(this).index());
+        $('.slider').animate({
+            left: -(index * 100) + "%"
 
-    $(function () {
+        }, 1000)
+    })
 
-        $('.point').click(function () {
-            var buttons = ($(this).index())
+    $(".arrow").click(function () {
+        if ($(this).hasClass("left-arrow")) {
+            // index = (index -1 + 5 = 4 ) % 5 = Le reste est égal à 0 donc il retourne à l'index 4 
+            index = (index + 4) % 5
             $('.slider').animate({
-                left: -(buttons * 100) + "%"
+                left: `-${(index * 100)}%`
+            }, 1000)
+        } else if ($(this).hasClass("right-arrow")) {
+            index = (index + 1) % 5
+            $('.slider').animate({
+                left: `-${(index * 100)}%`
 
             }, 1000)
+        }
+
+    })
+
+    setInterval(autoSlide, 5000)
+
+    function autoSlide() {
+        index = index + 1
+
+        if (index > 4) {
+            index = 0
+        }
+        $('.slider').animate({
+            left: `-${(index * 100)}%`
         })
-
-    })
-
-    $(".right-arrow").click(function () {
-        $(".slider").index() < 1 ?
-            $(".slider").animate({
-                left: ("-100%" * $(".slider").index().length) 
-            }) 
-        : console.log("va chier")    
-    })
+        randomImages()
+    }
 
 
+    function randomImages() {
+        $(".slide:nth-child()").each(function () {
+            $(this).css({
+            backgroundImage: "url(https://picsum.photos/1600/900)"
+        })
+        })
+    }
 
 
 })
